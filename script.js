eec8027c3017c8064a855200608e3c4bf2695b83
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-04 00:46:49 +0500

    Arrays

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
 //  Question 1 
 
-  document.write( "<h1>" + "Question 1" + "<br>" + "</h1>");
+document.write("<h1>" + "Question 1" + "<br>" + "</h1>");
+var students = [];
+
+document.write("Array");
+//   Question 2 
+
+document.write("<h1>" + "Question 2" + "<br>" + "</h1>");
+var studentNames = new Array();
+
+document.write("Array");
+
+// Question 3
+
+document.write("<h1>" + "Question 3" + "<br>" + "</h1>");
+var Fruits = ["Apple", "Mango", "Banana", "Strawberry", "Cherry"];
+
+document.write("Array");
+
+//   Question 4 
+
+document.write("<h1>" + "Question 4" + "<br>" + "</h1>");
+
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+document.write("Array");
+
+//   Question 5 
+
+document.write("<h1>" + "Question 5" + "<br>" + "</h1>");
+var flags = [true, false, true, false];
+
+document.write("Array");
+
+//  Question 6 
+
+document.write("<h1>" + "Question 6" + "<br>" + "</h1>");
+
+var mix = [1, "Ali", true];
+
+document.write("Array");
+
+//   Question 7
+
+document.write("<h1>" + "Question 7" + "<br>" + "</h1>");
+document.write("<h1>" + "Qualifications" + "<br>" + "</h1>");
+
+var degree = ["SSC", "HSC", "BCS", "BS", "B.COM", "M.Phil", "MS", "PhD"];
+
+document.write(("1") + degree[0] + "<br>");
+document.write(("2") + degree[1] + "<br>");
+document.write(("3") + degree[2] + "<br>");
+document.write(("4") + degree[3] + "<br>");
+document.write(("5") + degree[4] + "<br>");
+document.write(("6") + degree[5] + "<br>");
+document.write(("7") + degree[6] + "<br>");
+document.write(("8") + degree[7] + "<br>");
+
+
+// Question 8 
+
+document.write("<h1>" + "Question 8" + "<br>" + "</h1>");
+
+ var std_name = ["Ali", "Saqib", "Sajid"];
+ var score = [380 ,490 ,450];
+ var total = [500 , 500 , 500];
+
+ var percentage1 = (score[0]/total[0])*100;
+ var percentage2 = (score[1]/total[1])*100;
+ var percentage3 = (score[2]/total[2])*100;
+
+ document.write("Score of "+ std_name[0] + "is: " + score[0] + ".Percentage"+
+     percentage1 + "%<br>");
+      document.write("Score of "+ std_name[1] + "is: " + score[1] + ".Percentage"+
+     percentage2 + "%<br>");
+      document.write("Score of "+ std_name[2] + "is: " + score[2] + ".Percentage"+
+     percentage3 + "%");
+ 
